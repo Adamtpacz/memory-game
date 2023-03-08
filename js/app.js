950aa5ef33a5e10b595fdb89cf93a797b9ae260a
Adamtpacz <adamtpacz@gmail.com>
2023-03-08 10:57:15 -0500

    Define constants and add images

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
 console.log('Js Loaded')
 
 /*----- constants -----*/
+const cards = {
+    0: "Wizard",
+    1: "Dragon",
+    2: "Knight",
+    3: "Goblin",
+    4: "Princess",
+    5: "Dwarf",
+    6: "Troll",
+    7: "Castle"
+}
 
+const images = {
+    Wizard: "./images/wizard.png",
+    Dragon: "./images/dragon.png",
+    Knight: "./images/knight.jpeg",
+    Goblin: "./images/goblin.jpeg",
+    Princess: "./images/princess.jpeg",
+    Dwarf: "./images/dwarf.png",
+    Troll: "./images/troll.jpeg",
+    Castle: "./images/castle.png"
+}
+
+const colors = {
+    Visible: "white",
+    Hidden: "lightgreen"
+}
 
 /*----- state variables -----*/
 
